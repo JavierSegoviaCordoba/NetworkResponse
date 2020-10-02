9ac33f2c53b84cc999a99ceaa19937bd12cf527e
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-10-02 23:18:46 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ val versions: Versions get() = Versions
 
 object Versions {
     const val dependencyUpdates = "0.33.0"
-    const val detekt = "1.13.1"
+    const val detekt = "1.14.1"
     const val dokka = "1.4.10"
     const val coroutines = "1.3.9"
     const val kotest = "4.2.5"
@@ -11,6 +11,6 @@ object Versions {
     const val okHttp = "4.9.0"
     const val retrofit = "2.9.0"
     const val retrofitConverter = "0.7.0"
-    const val resource = "3.1.0.20200922220206-SNAPSHOT"
+    const val resource = "3.1.0.20201002202015-SNAPSHOT"
     const val serialization = "1.0.0-RC2"
 }
