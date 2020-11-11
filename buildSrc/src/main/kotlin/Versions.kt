1e03f5df8474eaae650ac495bd9614655fcafed0
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-11-11 01:34:54 +0100

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,13 @@
 val versions: Versions get() = Versions
 
 object Versions {
-    const val dependencyUpdates = "0.33.0"
+    const val dependencyUpdates = "0.36.0"
     const val detekt = "1.14.2"
     const val dokka = "1.4.10"
-    const val coroutines = "1.4.0"
-    const val kotest = "4.3.0"
+    const val coroutines = "1.4.1"
+    const val kotest = "4.3.1"
     const val kotlin = "1.4.10"
-    const val ktor = "1.4.1"
+    const val ktor = "1.4.2"
     const val nexus = "0.22.0"
     const val okHttp = "4.9.0"
     const val retrofit = "2.9.0"
