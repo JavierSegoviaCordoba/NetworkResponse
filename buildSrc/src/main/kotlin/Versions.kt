94906a9dbf373ce7768c828c54d2d8d98b328347
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-11-25 00:57:32 +0100

    Upgrade dependencies and Gradle

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val dokka = "1.4.10"
     const val coroutines = "1.4.1"
     const val kotest = "4.3.1"
-    const val kotlin = "1.4.10"
+    const val kotlin = "1.4.20"
     const val ktor = "1.4.2"
     const val nexus = "0.22.0"
     const val okHttp = "4.9.0"
